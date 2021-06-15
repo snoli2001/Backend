24d8b1e69bb7254d2d19dbfa78e97f04cfaf79c9
gsinuiri-coder <gsinuiri@gmail.com>
2021-06-14 23:34:40 -0500

    Add entitie message

---
+++ CarryDoggyGo.Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarryDoggyGo.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Text { get; set; }
+
+        public int DogWalkId { get; set; }
+        public virtual DogWalk DogWalk { get; set; }
+    }
+}
